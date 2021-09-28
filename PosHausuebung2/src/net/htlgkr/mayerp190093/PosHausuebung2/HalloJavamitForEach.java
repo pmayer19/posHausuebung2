875cd97f76a53031505bfb335d64364b1f550ee5
Paul <mayerp190093@sus.htl-grieskirchen.at>
2021-09-28 14:03:40 +0200

    Schuluebung2

---
+++ PosHausuebung2/src/net/htlgkr/mayerp190093/PosHausuebung2/HalloJavamitForEach.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htlgkr.mayerp190093.PosHausuebung2;
+
+import java.awt.BorderLayout;
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ *
+ * @author mayer
+ */
+public class HalloJavamitForEach {
+
+    public static void main(String[] args) {
+        List<String> liste = new ArrayList<String>(Arrays.asList());
+        liste.add("Enes");
+        liste.add("Fabi");
+        for (String string : liste) {
+            System.out.println(string);
+        }
+
+        liste.forEach((String s) -> System.out.println(s));
+
+        Consumer<String> consumer = (String s) -> System.out.println(s);
+
+    }
+}
